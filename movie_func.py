3112a2a11c07bc6ec919b5e05d3ec69ecfd60054
kujawa-konrad <kujawa.konrad.97@gmail.com>
2022-11-12 23:24:03 +0100

    New funcionality

--- movie_func.py
+++ movie_func.py
@@ -17,10 +17,17 @@ def MoviePicker(db):
             chosen = random.choice(test_list)
             return chosen
         elif dec == "Add":
+            name = input('Enter the name of the movie\n')
+            lenght = input('Enter the lenght of the movie in minutes\n')
+            genre = input('Enter the genre of the movie\n')
+            new_movie = Movie(name, lenght, genre)
 
-            break
+            # Here new entry will be added to db using Pandas
+            db.append(new_movie.name)
+            return f"You've added a {new_movie.name} movie to your database"
         else:
             return 'Unknown command'
 
 if __name__ == '__main__':
     print(MoviePicker(test_list))
+    print(test_list)
