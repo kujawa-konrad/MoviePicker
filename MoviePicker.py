36e15789002f4d0f9df91588b4ec1b4bf0eb7932
kujawa-konrad <kujawa.konrad.97@gmail.com>
2022-11-15 23:43:45 +0100

    Some fixes

--- MoviePicker.py
+++ MoviePicker.py
@@ -3,6 +3,7 @@
 from movie_func import MoviePicker
 import pandas as pd
 
+# Database used in app will be based on SQL and will be updated every time
 df = pd.DataFrame(columns=['Title', 'Lenght', 'Genre'])
 
 if __name__ == '__main__':
--- movie_func.py
+++ movie_func.py
@@ -1,42 +1,47 @@
 # Func to search a db
 
 from movie_methods import Movie
-import random
 import pandas as pd
 
-# test_list will be changed to dataframe
-# test_list = ['Inception', 'Pirates of th Caribbean', 'John Wick']
 
 def MoviePicker(db):
-
-    dec = input("Hello! What do you want me to do?\nPick / Add / Exit\n")
+    print('Hello!')
 
     while True:
+        dec = input("What do you want me to do?\nPick / Add / Exit\n")
+
         if dec == "Exit":
-            return 'Bye!'
+            print('Bye!')
+            break
         elif dec == "Pick":
-            # HAS TO BE CHANGED
-            chosen = random.choice(db)
-            return chosen
+
+            # Logic to pick by some params needed here
+            chosen = db.sample()['Title'].item()
+            print(f'Recommended movie is "{chosen}"')
+            break
         elif dec == "Add":
             title = input('Enter the title of the movie\n')
-            if title in db['Title']:
-                return 'This movie is already in the database'
+            if (db['Title'].eq(title)).any():
+                print('This movie is already in the database')
+                continue
             else:
                 lenght = input('Enter the lenght of the movie in minutes\n')
                 genre = input('Enter the genre of the movie\n')
             new_movie = Movie(title, lenght, genre)
 
-            # This does not update dataframe passed into the function
-            new_row = {'Title':new_movie.title, 'Lenght':new_movie.length, 'Genre':new_movie.genre}
-            db = db.append(new_row, ignore_index=True)
-            return f"You've added a {new_movie.title} movie to your database"
+            new_row = pd.DataFrame([[new_movie.title, new_movie.length, new_movie.genre]], columns=['Title', 'Lenght', 'Genre'])
+            db = pd.concat([db, new_row], ignore_index=True)
+            print(f"You've added a {new_movie.title} movie to your database")
+            return db
         else:
-            return 'Unknown command'
+            print('Unknown command')
 
 if __name__ == '__main__':
-    data = {'Title':['Inception', 'Pirates of th Caribbean', 'John Wick'], 'Lenght':['Long', 'Long', 'Long'], 'Genre':['Thriller', 'Adventure', 'Action']}
+    data = {'Title':['Inception', 'Pirates of the Caribbean', 'John Wick'], 'Lenght':['Long', 'Long', 'Long'], 'Genre':['Thriller', 'Adventure', 'Action']}
     test_df = pd.DataFrame(data)
-
-    print(MoviePicker(test_df))
+    db_check = MoviePicker(test_df)
+    if db_check is not None:
+        test_df = db_check
+    else:
+        pass
     print(test_df)
